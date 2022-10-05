705cb8ca6799ccff96b586b9d0d9d2779f93ae00
Gabriel-Evarist0 <machado.gabrielevaristo@gmail.com>
2022-10-05 00:09:49 -0300

    First Commit

--- src/Modulo_5/Atividade10M5.java
+++ src/Modulo_5/Atividade10M5.java
@@ -1,7 +1,21 @@
 package Modulo_5;
+import java.util.*;
 
 public class Atividade10M5 {
-    public static void main(String[] args) {
 
+    public static int fatorial(int x){
+        int i = x;
+        while (x > 1){
+            i = i *(x-1);
+            x--;
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Descubra o fatorial de um numero");
+        System.out.print("Digite o numero:");
+        int x = input.nextInt();
+        System.out.println("Fatorial de " + x + " é = " + fatorial(x));
     }
 }

2d58e8d118b4618e2c9310e8d409f6b242e1522a
Gabriel-Evarist0 <machado.gabrielevaristo@gmail.com>
2022-10-12 09:17:01 -0300

    Commited 10/10/2022 - 22:35

--- src/Procedimentos_e_Funcoes/Atividade2.java
+++ src/Procedimentos_e_Funcoes/Atividade2.java
@@ -2,25 +2,28 @@ package Procedimentos_e_Funcoes;
 import java.util.*;
 
 public class Atividade2 {
-
+//COMPARAÇÃO DE ESCOLHA
     public static void escolha(int choose, int a, int b){
         switch (choose){
+            //CASO 1 - PRINT SOMA
             case 1:
                 System.out.println("A soma de "+ a + " + "  + b + " = " + soma(a,b));
                 break;
+                //CASO 2 - PRINT SUBTRAÇÃO
             case 2:
                 System.out.println("A subtração de "+ a + " - "  + b + " = " + subtracao(a,b));
                 break;
+                //CASO 3 - MULTIPLICAÇÃO
             case 3:
                 System.out.println("A multiplicação de "+ a + " x "  + b + " = " + multiplicacao(a,b));
         }
 
     }
-
+    //AÇÃO DE SOMA
     public static int soma(int a, int b){
         return a + b;
     }
-
+    //AÇÃO DE MULTIPLICAÇÃO
     public static int multiplicacao(int a, int b){
         int mult = 0;
         for (int i = 0; i < b;i++){
@@ -29,10 +32,12 @@ public class Atividade2 {
 
         return mult;
     }
+    //AÇÃO DE SUBTRAÇÃO
     public static int subtracao(int a, int b){
         return a - b;
     }
 
+    //"Menu"
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("--------------------------");
@@ -49,6 +54,7 @@ public class Atividade2 {
         System.out.println("3 - Multiplicação");
         System.out.println("--------------------------");
         int choose = input.nextInt();
+        //CHAMA AÇÃO DE COMPARAÇÃO DE ESCOLHA
         escolha(choose, a, b);
     }
 
